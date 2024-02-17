ae5083af2f61e8d558009c38348cd3a64a3ddfea
kunalkc1111 <157978131+kunalkc1111@users.noreply.github.com>
2024-02-18 00:13:33 +0530

    add

--- practice/forms.py
+++ practice/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import FillDetailsModels
+from .models import FillDetailsModels, SignUpModel
 
 class FillDetailsForm(forms.ModelForm):
     class Meta:
         model = FillDetailsModels
+        fields = '__all__'
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = SignUpModel
         fields = '__all__'
--- practice/models.py
+++ practice/models.py
@@ -3,11 +3,14 @@ from django.db import models
 # Create your models here.
 class FillDetailsModels(models.Model):
     sno = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone_no = models.CharField(max_length=10)
+    full_name = models.CharField(max_length=100)
     email = models.CharField(max_length=50)
-    file_detail = models.TextField()
     fileInput = models.FileField(upload_to='media/')
     timestamp = models.DateTimeField(auto_now_add=True, blank=True)
 
+class SignUpModel(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    username = models.CharField(max_length=100)
+    pass1 = models.CharField(max_length=10)
+    pass2 = models.CharField(max_length=10)
